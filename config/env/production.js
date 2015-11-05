bd4063c91dd90690fc987301305676e873babc39
oialvarez <oialvarez@gmail.com>
2015-11-05 00:46:49 -0300

    agrega configuracion de ambientes nginx

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
+  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/viajante-prod',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
@@ -28,7 +28,7 @@ module.exports = {
   },
   hostname: 'http://localhost:3000',
   app: {
-    name: 'MEAN - A Modern Stack - Production'
+    name: 'Viajante - production'
   },
   logging: {
     format: 'combined'
