113dd0889fed42bd1476e82ab000a78019eaba06
oialvarez <oialvarez@gmail.com>
2015-11-11 12:59:39 -0300

    elimino package hospedaje

--- packages/custom/hospedaje/server/routes/hospedaje.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* jshint -W098 */
-// The Package is past automatically as first parameter
-module.exports = function (Hospedaje, app, auth, database) {
-    app.get('/api/hospedaje/', function (req, res, next) {
-        res.send('Anyone can access this');
-    });
-};
