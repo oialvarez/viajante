8d8af96e52699a3e7630a57d28664ca07a8e6d9c
oialvarez <oialvarez@gmail.com>
2015-11-19 16:13:43 -0300

    elimino menu mean upload

--- packages/contrib/upload/app.js
+++ packages/contrib/upload/app.js
@@ -31,12 +31,12 @@ Upload.register(function(app, auth, database) {
     Upload.angularDependencies(['angularFileUpload']);
 
     //We are adding a link to the main menu for all authenticated users
-    Upload.menus.add({
+    /*Upload.menus.add({
         title: 'Mean upload help page',
         link: 'Mean upload help page',
         roles: ['authenticated'],
         menu: 'main'
-    });
+    });*/
 
     if (Upload.events.defaultData)
         Upload.events.defaultData({
